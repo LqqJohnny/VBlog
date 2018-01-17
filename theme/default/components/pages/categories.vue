4ca3f6fa84ff26b0c50da5b0291352c94a8fc9d4
lqq <1277601297@qq.com>
2018-01-17 10:32:10 +0800

    添加tags页

--- theme/default/components/pages/categories.vue
+++ theme/default/components/pages/categories.vue
@@ -1,43 +1,64 @@
 <template lang="html">
 	<div class="category_box">
-		<h3 class="cate_title">Categories</h3>
+		<h3 class="cate_title">分类</h3>
 		<div class="cate_list">
-			<ul>
-				<li v-for="(val,key) in categories"><a>{{key}}</a> <span>({{val}})</span></li>
+			<ul class="clearfix">
+				<li  v-for="(val,key) in categories" class="cate_item" :class="{select : selectCate == key}" @click="showAtcs(val,key)">
+					<a>{{key}}</a><span class="artNum">({{val.length}})</span>
+				</li>
 			</ul>
+			<div v-if="showArticles.length>0">
+				<hr>
+				<ul>
+					<li class="article" v-for="art in showArticles">
+						<router-link :to="genUrl(art)" >{{genName(art)}}</router-link>
+					</li>
+				</ul>
+			</div>
 		</div>
 	</div>
 </template>
 <script>
-var articles = require('../../../../articles.json');
+var categories = require('../../../../categories.json');
 export default {
 	data(){
 		return {
-			categories:[]
+			categories: categories,
+			showArticles:[],
+			selectCate:""
 		}
 	},
-	mounted(){
-		var _this = this,categories = {};
-		articles.forEach(function(val){
-			if (typeof categories[val.categories] !='undefined') {
-				categories[val.categories] ++;
-			}else{
-				categories[val.categories] = 1;
-			}
-		})
-		this.categories = categories;
+	methods:{
+		showAtcs(val,key){
+			this.showArticles = val;
+			this.selectCate = key;
+		},
+		genUrl(name){
+			return '/article/'+name.replace(/\.md/g,'')
+		},
+		genName(name){
+			return name.replace(/\.md/g,'')
+		}
 	}
 }
 </script>
-<style type="text/css">
+<style type="text/css" scoped>
+hr {
+		border-color: #2c8898;
+}
+hr {
+		box-sizing: content-box;
+		height: 0;
+		overflow: visible;
+}
 	.category_box{font-family: 'Lato', "PingFang SC", "Microsoft YaHei", sans-serif;}
 	.cate_title{
 		text-align: center;
 		color: #555;
-		margin: 20px 0 10px;
+		margin: 1rem 0 .5rem;
 	}
 	.cate_list li{
-		margin: 5px 10px;
+		margin: 0.2rem 0.5rem;
 		line-height: 2;
 	}
 	.cate_list li a{
@@ -47,4 +68,18 @@ export default {
 		cursor: pointer;
 	}
 	.cate_list li span{color: #bbb;}
+	.cate_item{
+		display: inline-block;
+	}
+	.cate_item.select a{
+		color: #428bca;
+		font-weight: bold;
+		border-color: #428bca;
+	}
+	.article a{
+		text-decoration: none;
+	}
+	.artNum{
+		color:rgb(177, 177, 177);
+	}
 </style>
---
+++ theme/default/components/pages/tags.vue
@@ -0,0 +1,91 @@
+<template lang="html">
+	<div class="category_box">
+		<h3 class="cate_title">标签</h3>
+		<div class="cate_list">
+			<ul class="clearfix">
+				<li   v-for="(val,key) in tags"
+              class="cate_item"
+              :class="{select : selectTag == key}"
+              @click="showAtcs(val,key)">
+					<a :style="{fontSize : genFontSize(val.length)}">{{key}}</a>
+				</li>
+			</ul>
+			<div v-if="showArticles.length>0">
+				<hr>
+				<ul>
+					<li class="article" v-for="art in showArticles">
+						<router-link :to="genUrl(art)" >{{genName(art)}}</router-link>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+var tags = require('../../../../tags.json');
+export default {
+	data(){
+		return {
+			tags: tags,
+			showArticles:[],
+			selectTag:""
+		}
+	},
+	methods:{
+    genFontSize(length){
+      var size = length*0.3;
+      size = Math.min(size,2);
+      size = Math.max(size,1);
+      return size+'rem';
+    },
+		showAtcs(val,key){
+			this.showArticles = val;
+			this.selectTag = key;
+		},
+		genUrl(name){
+			return '/article/'+name.replace(/\.md/g,'')
+		},
+		genName(name){
+			return name.replace(/\.md/g,'')
+		}
+	}
+}
+</script>
+<style type="text/css" scoped>
+  hr {
+      border-color: #2c8898;
+  }
+  hr {
+      box-sizing: content-box;
+      height: 0;
+      overflow: visible;
+  }
+	.category_box{font-family: 'Lato', "PingFang SC", "Microsoft YaHei", sans-serif;}
+	.cate_title{
+		text-align: center;
+		color: #555;
+		margin: 1rem 0 .5rem;
+	}
+	.cate_list li{
+		margin: 0.2rem 0.5rem;
+		line-height: 2;
+	}
+	.cate_list li a{
+		color: #555;
+		border-bottom: 1px solid #ccc;
+		font-size: 1rem;
+		cursor: pointer;
+	}
+	.cate_list li span{color: #bbb;}
+	.cate_item{
+		display: inline-block;
+	}
+	.cate_item.select a{
+		color: #428bca;
+		font-weight: bold;
+		border-color: #428bca;
+	}
+	.article a{
+		text-decoration: none;
+	}
+</style>
